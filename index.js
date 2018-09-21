1d122eaa4709bc281c01a3772da718544db1e646
zhaoying <yzhao96@best-inc.com>
2018-09-21 15:57:03 +0800

    update file copy logic

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-
-var config = {};
-
-ProcessingInstruction.argv.slice(2).forEach(function (item) {
-  switch (item) {
-    case "-j":
-      config.jquery = true;
-      break;
-    case "-s":
-      config.swiper = true;
-      break;
-    case "-v":
-      config.vue = true;
-      break;
-    case "-b":
-      config.bootstrap = true;
-      break;
-  }
-});
+const fs = require('fs');
+const path = require('path');
+const mkdirPath = './templates';
 
 function copyTemplate ( from , to ) {
   from = path.join(__dirname, 'templates', from);
   write(to, fs.readFileSync(from, 'utf-8'));
+}
+
+function copyCatalog (mkdirPath) {
+  fs.readdir(mkdirPath, (err, files) => {
+    if (err) {
+      console.log(`readdir ${mkdirPath} file failed ~`);
+      return false;
+    }
+
+
+    mkdir(`${mkdirPath}`, () => {
+      files.forEach(item => {
+        fs.stat(`${mkdirPath}/${item}`, function(err,stat){
+          if (err) {
+            console.error(err);
+            throw err;
+          }
+  
+          if (stat.isFile()) {
+            copyTemplate(`${mkdirPath}/${item}`, `${mkdirPath}/${item}`);
+          } else if (stat.isDirectory()) {
+            copyCatalog(`${mkdirPath}/${item}`, `${mkdirPath}/${item}`);
+          }
+        });
+      });
+    });
+
+  });
 }
 
 function write ( path, str, mode ) {
@@ -37,25 +47,4 @@ function mkdir ( path, fn ) {
   });
 }
 
-var PATH = ".";
-mkdir (PATH, function () {
-  copyTemplate('index.html', PATH);
-  copyTemplate('index.css', PATH);
-});
-mkdir (PATH + '/libs/js', function () {
-  config.bootstrap && copyTemplate('js/bootstrap.min.js', PATH + "/libs/js/bootstrap.min.js");
-  config.bootstrap && copyTemplate('js/vue.min.js', PATH + "/libs/js/vue.min.js");
-
-  if (config.jquery) {
-    config.bootstrap && copyTemplate('js/jquery-1.8.3.min.js', PATH + "/libs/js/jquery-1.8.3.min.js");
-    config.bootstrap && copyTemplate('js/jquery-3.1.1.min.js', PATH + "/libs/js/jquery-3.1.1.min.js");
-  }
-
-  if (config.swiper) {
-    if (config.jquery) {
-      config.bootstrap && copyTemplate('js/swiper-3.4.1.min.js', PATH + "/libs/js/swiper-3.4.1.min.js");
-    } else {
-      config.bootstrap && copyTemplate('js/swiper-3.4.2.min.js', PATH + "/libs/js/swiper-3.4.2.min.js");
-    }
-  }
-});
+copyCatalog(mkdirPath);
