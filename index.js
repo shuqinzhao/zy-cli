e2c776fc39701a6c2d8a058ab052d280825cafd9
zhaoying <yzhao96@best-inc.com>
2018-09-30 14:50:56 +0800

    [feat] add dynamic naming the project name

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const path = require('path');
+const program = require('commander');
+const package = require(path.resolve(__dirname, './package.json'));
 const BASE_PATH = './templates';
 
 function copyTemplate ( from , to ) {
@@ -9,12 +11,12 @@ function copyTemplate ( from , to ) {
   write(to, fs.readFileSync(from, 'utf-8'));
 }
 
-function copyCatalog (mkdirPath) {
+function copyCatalog (mkdirPath, projectName) {
   from = path.join(__dirname, mkdirPath);
 
   fs.readdir(from, (err, files) => {
     if (err) {
-      console.log(`readdirSync ${from} file failed ~`);
+      console.log(`   readdirSync ${from} file failed ~`);
       return false;
     }
 
@@ -27,16 +29,17 @@ function copyCatalog (mkdirPath) {
           }
   
           if (stat.isFile()) {
-            copyTemplate(`${mkdirPath}/${item}`, item);
+            console.log(`   copy ${mkdirPath}/${item} to ./${projectName}/${item}`);
+            copyTemplate(`${mkdirPath}/${item}`, `./${projectName}/${item}`);
           } else if (stat.isDirectory()) {
-            copyCatalog(`${mkdirPath}/${item}`, item);
+            copyCatalog(`${mkdirPath}/${item}`, projectName);
           }
         });
       });
     } else {
       const path = mkdirPath.split(BASE_PATH)[1];
-      
-      mkdir(`.${path}`, () => {
+
+      mkdir(`./${projectName}${path}`, () => {
         files.forEach(item => {
           fs.stat(`${from}/${item}`, function(err,stat) {
             if (err) {
@@ -45,9 +48,10 @@ function copyCatalog (mkdirPath) {
             }
     
             if (stat.isFile()) {
-              copyTemplate(`${mkdirPath}/${item}`, `.${path}/${item}`);
+              copyTemplate(`${mkdirPath}/${item}`, `./${projectName}${path}/${item}`);
+              console.log(`   copy ${mkdirPath}/${item} to ./${projectName}${path}/${item}`);
             } else if (stat.isDirectory()) {
-              copyCatalog(`${mkdirPath}/${item}`, `.${path}/${item}`);
+              copyCatalog(`${mkdirPath}/${item}`, projectName);
             }
           });
         });
@@ -66,4 +70,28 @@ function mkdir ( path, fn ) {
   });
 }
 
-copyCatalog(BASE_PATH);
+function printHelp () {
+  console.log(' Examples');
+  console.log(' ');
+  console.log('   交互创建项目');
+  console.log('   bestinc-cli');
+  console.log(' ');
+}
+
+program.version(package.version)
+  .usage('[options]')
+  .option('-C, --create <project-name>', '指定项目名称');
+
+program.on('--help', printHelp);
+program.parse(process.argv);
+
+if (program.create) {
+  console.log(' ');
+  console.log('   创建项目目录：');
+  console.log(' ');
+  console.log('     creating %s', program.create);
+  console.log(' ');
+  mkdir(`./${program.create}`, () => {
+    copyCatalog(BASE_PATH, program.create);
+  });
+}
